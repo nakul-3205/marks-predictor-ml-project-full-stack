2b9963d34764b454cfde66be9afe1f57658ba6c8
nakul-3205 <nakulkejriwal079@gmail.com>
2025-08-04 15:11:50 +0530

    app completed

--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,7 @@ import dill
 from src.exception import CustomException
 from sklearn.metrics import r2_score
 from sklearn.model_selection import GridSearchCV
-
+import pickle
 def save_Object(file_path,obj):
     try:
         dir_path=os.path.dirname(file_path)
@@ -49,3 +49,10 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
     except Exception as e:
         raise CustomException(e, sys)
     
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
